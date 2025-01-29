67885586a6f511543a161df7bb9ac6afd07baaec
ChillerDragon <chillerdragon@gmail.com>
2025-01-29 15:12:48 +0100

    tmp server log for tailing it on max line reach

--- lib/setup_sudo.sh
+++ lib/setup_sudo.sh
@@ -84,7 +84,7 @@ function setup_apache_log_server() {
 crashes.txt,donations_sum.html,donations.txt,\
 full_gdb.txt,log_gdb.txt,money.txt,\
 ddos.txt,\
-server_log.txt,\
+server_log.txt,server_log.txt.tmp\
 paste.txt,raw_build.txt,status.txt,gprof.svg,\
 cpu.html,querys.html,index.html}
 
